cd949c67feee485863d0f29fd3bb596f2e3c46c1
David Bowers <dbowers.github.com>
2017-04-17 11:19:52 -0600

    change debug level

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,7 @@ use Mix.Config
 #
 # Or configure a 3rd-party app:
 #
-    config :logger, level: :error, path: "log/skynet.log"
+    config :logger, level: :info, path: "log/skynet.log"
 #
 
 # It is also possible to import configuration files, relative to this
